6e58d867797729d9a9c4e69b60521deb5bdd9ab6
Anusha pade <padeanusha@gmail.com>
2025-09-23 02:38:06 +0000

    change logo into plant

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,6 +4,8 @@ export function KisanMitraIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -12,10 +14,10 @@ export function KisanMitraIcon(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M11 20A7 7 0 0 1 4 13V6a2 2 0 0 1 2-2h4l2 3h6a2 2 0 0 1 2 2v2.344" />
-      <path d="M14.29 21.71a2.1 2.1 0 0 0 2.97 0l2.44-2.44a2.1 2.1 0 0 0 0-2.97l-2.44-2.44a2.1 2.12 0 0 0-2.97 0L14.29 16" />
-      <path d="m7 13-3.07 3.07a2.1 2.1 0 0 0 0 2.97L7 22" />
-      <path d="M12.5 15.5 16 12" />
+      <path d="M7 20h10" />
+      <path d="M12 20V4" />
+      <path d="M12 4H8c-2 0-4 2-4 4v0c0 2 2 4 4 4h4" />
+      <path d="M12 4h4c2 0 4 2 4 4v0c0 2-2 4-4 4h-4" />
     </svg>
   );
 }
